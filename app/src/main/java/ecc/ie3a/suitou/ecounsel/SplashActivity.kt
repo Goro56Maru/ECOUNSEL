d142731f3013a0438d97b400b88e4f73c1e92a21
Goro56Maru <93966202+Goro56Maru@users.noreply.github.com>
2022-11-01 14:18:36 +0900

    SplashActivity(起動時の待機画面)の作成

---
+++ app/src/main/java/ecc/ie3a/suitou/ecounsel/SplashActivity.kt
@@ -0,0 +1,13 @@
+package ecc.ie3a.suitou.ecounsel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //テスト
+    }
+}
