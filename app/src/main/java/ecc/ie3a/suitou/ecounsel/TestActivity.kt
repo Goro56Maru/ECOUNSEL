e67833080f790b43ce090f9c7afd6036aeaba333
2200082 <koco0828imo@gmail.com>
2022-11-15 09:56:39 +0900

    テスト用

--- app/src/main/java/ecc/ie3a/suitou/ecounsel/TestActivity.kt
+++ app/src/main/java/ecc/ie3a/suitou/ecounsel/TestActivity.kt
@@ -3,12 +3,9 @@ package ecc.ie3a.suitou.ecounsel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //テストよう
-
+        setContentView(R.layout.activity_test)
     }
 }
