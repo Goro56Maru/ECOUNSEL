6da1cd842bcaed9aeb9c75f5425ef4553620f252
Goro56Maru <93966202+Goro56Maru@users.noreply.github.com>
2022-11-17 14:05:29 +0900

    SplashActivityの修正

--- app/src/main/java/ecc/ie3a/suitou/ecounsel/SplashActivity.kt
+++ app/src/main/java/ecc/ie3a/suitou/ecounsel/SplashActivity.kt
@@ -5,15 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.WindowManager
+
 
 class SplashActivity : AppCompatActivity() {
-    private var SPLASH_TIME:Long=2000
+    private var SPLASH_TIME:Long=4000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.myLooper()!!).postDelayed({
